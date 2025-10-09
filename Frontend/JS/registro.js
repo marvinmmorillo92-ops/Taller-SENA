a7530a37cbf7375705ae880858299ce85f54a8d2
Marvin_Morillo <marvinmmorillo92@gmail.com>
2025-10-08 23:08:50 -0500

    funcion para mostrar mensaje de alerta

--- Frontend/JS/registro.js
+++ Frontend/JS/registro.js
@@ -1,6 +1,24 @@
 document.addEventListener("DomContentLoaded", () =>{
 
+const formRegistro = document.getElementById("formRegistro");
+const API_URL = "http://localhost:4000/api/usuarios";
 
+// Función para mostra mensaje de alerta
+function showAlert(message, type = "info") {
+    const existing = document.getElementById("formAlert");
+    if (existing) existing.remove();
+
+    const div = document.createElement("div");
+    div.id = "formAlert";
+    div.className = `alert alert-${type} mt-3`;
+    div.role = "alert";
+    div.textContent = message;
+    form.prepend(div);
+
+    setTimeout(() => {
+       if (div.parentNode) div.remove();
+    }, 5000);
+}
 
 
 });
