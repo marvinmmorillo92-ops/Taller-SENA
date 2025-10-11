cdb95c720a67a241370c682be5365c69fbd2d6eb
Marvin_Morillo <marvinmmorillo92@gmail.com>
2025-10-11 16:30:03 -0500

    Muestra informacion del usuario

--- Frontend/JS/perfil.js
+++ Frontend/JS/perfil.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    
+    //leer el usuario del localStorage
+    const usuario = JSON.parse(localStorage.getItem("usuario"));
 
+    if (!usuario) {alert ("No has iniciado sesión. Redirigiendo al login.");
+    window.location.href = "./perfil.html"; return; }   // me regresa al perfil para hacer el login
 
+// Mostrar datos del usuario en el perfil
+document.getElementById("nombreUsuario").textContent = usuario.nombre || "N/A";
+document.getElementById("correoUsuario").textContent = usuario.correo || "N/A";
+document.getElementById("telefonoUsuario").textContent = usuario.telefono || "N/A";
+document.getElementById("ciudadUsuario").textContent = usuario.ciudad || "N/A";
+document.getElementById("direccionUsuario").textContent = usuario.direccion || "N/A";
 
 });
