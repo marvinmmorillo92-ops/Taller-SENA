03b0e9932e234b282ca6013dffccc22fbe93e2bf
Marvin_Morillo <marvinmmorillo92@gmail.com>
2025-10-13 03:01:40 -0500

    AJUSTES PARA EL CIERRE DE SESION

--- Frontend/JS/session.js
+++ Frontend/JS/session.js
@@ -2,12 +2,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const usuario = JSON.parse(localStorage.getItem("usuario"));
   const nombreSesion = document.getElementById("nombreSesion");
-const cerrarSesionHeader = document.getElementById("cerrarSesionBtn");
+const cerrarSesionHeader = document.getElementById("cerrarSesionHeader");
 
   if (usuario && nombreSesion) {
     // Mostrar saludo o nombre corto
-    const primerNombre = usuario.nombres.split(" ")[0].tolowerCase();
-    nombreSesion.textContent = `Hola, ${primerNombre.charAt(0).toUpperCase() + primerNombre.slice(1)}`;
+     const primerNombre = usuario.nombres.split(" ")[0];
+    nombreSesion.textContent = `Hola, ${primerNombre.toUpperCase()}`;
   } else if (nombreSesion) {
 
     nombreSesion.textContent = "Iniciar sesión";
@@ -19,8 +19,32 @@ if (cerrarSesionHeader){
     cerrarSesionHeader.addEventListener("click", (e) => {
         e.preventDefault();
         localStorage.removeItem("usuario");
-        window.location.href = "../HTML/login.html";
+
+        //Detecta ruta actual
+
+        let loginPath = "./HTML/login.html";
+if (window.location.pathname.includes("/HTML/")) {
+loginPath = "../HTML/login.html";
+
+}
+
+Window.location.herf = loginPath;
+
     });
 }
+
+// Forzar login si no hay usuario en localStorage
+const paginasProtegidas = ["/HTML/perfil.html", "/HTML/carrito.html"];
+const paginaActual = window.location.pathname.split("/").pop();
+
+if (!usuario && paginasProtegidas.includes(paginaActual)) {
+    alert("Debes iniciar sesion, Redirigiendo al login...");
+    const loginPath = window.location.pathname.includes("/HTML/") ? "./login.html" : "./HTML/login.html";
+    
+    loginPath = "../login.html";
+    window.location.href = loginPath;    
+}
+
+
 
 });
