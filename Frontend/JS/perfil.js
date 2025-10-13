99530357e7ef1f7bae8fb9a1a87e449ad145899e
Marvin_Morillo <marvinmmorillo92@gmail.com>
2025-10-12 22:29:29 -0500

    Se ajustan errores en perfil.js

--- Frontend/JS/perfil.js
+++ Frontend/JS/perfil.js
@@ -3,13 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     //leer el usuario del localStorage
     const usuario = JSON.parse(localStorage.getItem("usuario"));
 
-    if (!usuario) {alert ("No has iniciado sesión. Redirigiendo al login.");
+    if (!usuario) {alert ("No has iniciado sesión. Redirigiendo al login...");
     window.location.href = "./login.html"; return; }   // me regresa para hacer el login
 
 // Mostrar datos del usuario en el perfil
-document.getElementById("nombreUsuario").textContent = usuario.nombre || "N/A";
+document.getElementById("nombreUsuario").textContent = usuario.nombres || "N/A";
 document.getElementById("correoUsuario").textContent = usuario.correo || "N/A";
-document.getElementById("telefonoUsuario").textContent = usuario.telefono || "N/A";
 document.getElementById("ciudadUsuario").textContent = usuario.ciudad || "N/A";
 document.getElementById("direccionUsuario").textContent = usuario.direccion || "N/A";
 
