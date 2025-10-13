8c49dd55592dd76f04a79d1dabd740dc12a2bb40
Marvin_Morillo <marvinmmorillo92@gmail.com>
2025-10-13 01:11:33 -0500

    SE ACTUALIZA session

--- Frontend/JS/session.js
+++ Frontend/JS/session.js
@@ -2,10 +2,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   const usuario = JSON.parse(localStorage.getItem("usuario"));
   const nombreSesion = document.getElementById("nombreSesion");
+const cerrarSesionHeader = document.getElementById("cerrarSesionBtn");
 
   if (usuario && nombreSesion) {
     // Mostrar saludo o nombre corto
-    const primerNombre = usuario.nombres.split(" ")[0];
-    nombreSesion.textContent = `Hola, ${primerNombre}`;
+    const primerNombre = usuario.nombres.split(" ")[0].tolowerCase();
+    nombreSesion.textContent = `Hola, ${primerNombre.charAt(0).toUpperCase() + primerNombre.slice(1)}`;
+  } else if (nombreSesion) {
+
+    nombreSesion.textContent = "Iniciar sesión";
+
   }
+// Cerrar sesión desd e el header
+if (cerrarSesionHeader){
+
+    cerrarSesionHeader.addEventListener("click", (e) => {
+        e.preventDefault();
+        localStorage.removeItem("usuario");
+        window.location.href = "../HTML/login.html";
+    });
+}
+
 });
