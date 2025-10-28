7ccf69875ffeb4569f49f7c0e0dfd308d3f3382f
Marvin_Morillo <marvinmmorillo92@gmail.com>
2025-10-28 01:20:34 -0500

    db.js

---
+++ Backend/db.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2");
+require("dotenv").config(); //Cargar variables del .env
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
+  queueLimit: 0
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error en la conexión:", err);
+    return;
+  }
+  console.log("Conexión exitosa");
+  connection.release();
+});
+
+module.exports = pool;
